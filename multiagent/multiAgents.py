9f447c4424898dfce5b75f4296a55b14dd516b75
Martin Glusker <martin_glusker@vlan408-219.subnet-247.amherst.edu>
2019-10-04 13:58:46 -0400

    q2 5/5 lets go

--- multiagent/multiAgents.py
+++ multiagent/multiAgents.py
@@ -163,16 +163,21 @@ class MultiAgentSearchAgent(Agent):
       only partially specified, and designed to be extended.  Agent (game.py)
       is another abstract class.
     """
+    
 
     def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
         self.index = 0 # Pacman is always agent index 0
         self.evaluationFunction = util.lookup(evalFn, globals())
         self.depth = int(depth)
+        
+
 
 class MinimaxAgent(MultiAgentSearchAgent):
+
     """
       Your minimax agent (question 2)
     """
+  
 
     def getAction(self, gameState):
         """
@@ -191,8 +196,84 @@ class MinimaxAgent(MultiAgentSearchAgent):
           gameState.getNumAgents():
             Returns the total number of agents in the game
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+
+        #It's necessarily pacman's turn cause this is at the root 
+
+        pacmanSuccessors = [] #list of GameStates
+        pacmanSuccessorsEvalScores = [] #list of GameStates returned scores
+
+        pacmanLegalActions = gameState.getLegalActions(self.index)
+
+        for action in pacmanLegalActions:
+          pacmanSuccessors.append(gameState.generateSuccessor(self.index, action))
+        
+        for child in pacmanSuccessors:
+          pacmanSuccessorsEvalScores.append(self.getActionRecursiveHelper(child, 1))
+
+        #print "*****LENGTH*******"
+        #print len(pacmanLegalActions)
+        #print len(pacmanSuccessors)
+        #print len(pacmanSuccessorsEvalScores)
+        return pacmanLegalActions[pacmanSuccessorsEvalScores.index(max(pacmanSuccessorsEvalScores))]
+     
+
+
+    def getActionRecursiveHelper(self, gameState, depthCounter):
+
+      numAgents = gameState.getNumAgents()
+
+      #cutoff test
+      if((self.depth*numAgents) == depthCounter):
+        return self.evaluationFunction(gameState)
+
+      #implement a terminal test
+      if(gameState.isWin() ==True or gameState.isLose() == True):
+        return self.evaluationFunction(gameState)
+
+      #print "depthCounter: ", depthCounter
+
+      # When it's pacman's turn
+      if((depthCounter%numAgents) == self.index): 
+        pacmanSuccessors = [] #list of GameStates
+        pacmanSuccessorsEvalScores = [] #list of GameStates returned scores
+
+        pacmanLegalActions = gameState.getLegalActions(self.index)
+
+        #print "length Legal Actions: ", len(pacmanLegalActions)
+
+        for action in pacmanLegalActions:
+          pacmanSuccessors.append(gameState.generateSuccessor(self.index, action))
+        
+        #print "length of pacman successors: ", len(pacmanSuccessors)
+
+        for child in pacmanSuccessors:
+          pacmanSuccessorsEvalScores.append(self.getActionRecursiveHelper(child, depthCounter+1))
+
+
+        #print "length of pacman eval scores: ", len(pacmanSuccessorsEvalScores)
+
+        return max(pacmanSuccessorsEvalScores)
+
+      #It's a ghost turn
+      else: 
+        ghostNumber = (depthCounter%numAgents) #which ghost is it?
+
+        ghostSuccessors = [] #list of GameStates
+        ghostSuccessorsEvalScores = [] #list of GameStates returned scores
+
+        ghostLegalActions = gameState.getLegalActions(ghostNumber)
+
+        for action in ghostLegalActions:
+          ghostSuccessors.append(gameState.generateSuccessor(ghostNumber, action))
+        
+
+        for child in ghostSuccessors:
+          ghostSuccessorsEvalScores.append(self.getActionRecursiveHelper(child, depthCounter+1))
+
+      
+        return min(ghostSuccessorsEvalScores)
+
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
@@ -222,14 +303,90 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         util.raiseNotDefined()
 
 def betterEvaluationFunction(currentGameState):
-    """
-      Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
-      evaluation function (question 5).
+   
 
-      DESCRIPTION: <write something here so we know what you did>
-    """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+        """
+        Design a better evaluation function here.
+
+        The evaluation function takes in the current and proposed successor
+        GameStates (pacman.py) and returns a number, where higher numbers are better.
+
+        The code below extracts some useful information from the state, like the
+        remaining food (newFood) and Pacman position after moving (newPos).
+        newScaredTimes holds the number of moves that each ghost will remain
+        scared because of Pacman having eaten a power pellet.
+
+        Print out these variables to see what you're getting, then combine them
+        to create a masterful evaluation function.
+        """
+        # Useful information you can extract from a GameState (pacman.py)
+        successorGameState = currentGameState.generatePacmanSuccessor(action)
+        newPos = successorGameState.getPacmanPosition()
+        newFood = successorGameState.getFood()
+        newGhostStates = successorGameState.getGhostStates()
+        newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+
+
+        foodList = newFood.asList()
+        foodDistances = []
+        finalScore = 0.0
+
+        # find the manhattan distance to each pellet of food
+        for food in foodList:    
+            foodDistances.append(manhattanDistance(successorGameState.getPacmanPosition(), food))
+
+        # if there's no food left in the game
+        if(len(foodDistances) == 0): 
+          # if the next state will result in there being 0 pellets of food left, take that move
+          return finalScore + 100000000
+         
+        else:
+          # get the closest food and scale it up to make it more desireable
+          closestFoodDistance = min(foodDistances) 
+          closestFoodScore = (1.0/closestFoodDistance) * 500.0
+
+
+        #creates a list of distances to ghosts
+        distanceToGhosts = []
+        for ghost in newGhostStates:
+          distanceToGhosts.append(manhattanDistance(successorGameState.getPacmanPosition(), ghost.getPosition()))
+
+        # manhattan distance to the closest ghost
+        closestGhostDistance = min(distanceToGhosts)
+  
+
+        # if pacman is vulnerable to ghosts or is about to become vulnerable (arbitrary cut off is 3 seconds)
+        if(min(newScaredTimes) <= 3):
+
+          # if the next spot is  food, GOOD, increase score
+          if(len(newFood.asList()) < len(currentGameState.getFood().asList())):
+            finalScore += 1000
+
+
+          # if pacman is close to a ghost, BAD, decrement score 
+          if(closestGhostDistance <= 3):
+            if(closestGhostDistance == 0):
+              finalScore -= float("inf")
+            if(closestGhostDistance == 1):
+              finalScore -= 10000
+            if(closestGhostDistance == 2):
+              finalScore -= 8000
+            if(closestGhostDistance == 3):
+              finalScore -= 7000
+
+          # otherwise pacman is at least 4 spaces away
+          else:
+            # so reward the closest food spot 
+            finalScore += closestFoodScore
+
+        # otherwise pacman can go wherever, so go straight for food without care for ghosts
+        else:
+          finalScore += closestFoodScore
+          # if the next spot is  food, GOOD, increase score
+          if(len(newFood.asList()) < len(currentGameState.getFood().asList())):
+            finalScore += 1000
+
+        return finalScore
 
 # Abbreviation
 better = betterEvaluationFunction
